2efeff0decca28087b8344fa8604040462a327b2
Gülay Kızılgedik <164217083+gulaykgedik@users.noreply.github.com>
2025-12-22 19:59:54 +0300

    Remove old .jsx and .js files after TypeScript migration

--- src/theme/colors.js
+++
@@ -1,9 +0,0 @@
-const Colors = {
-  BLACK: '#000000',
-  WHITE: '#FFFFFF',
-  RED: '#C60A0A',
-  YELLOW: '#FFD700',
-  GRAY: '#b8b4b4ff',
-};
-
-export default Colors;
--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-import { Dimensions } from "react-native";
-
-const screenWidth = Dimensions.get("window").width;
-const screenHeight = Dimensions.get("window").height;
-const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTg5ODhkMjFkZGNiYzY1MzJhNDg0OTYzZmRiYWM3ZSIsIm5iZiI6MTczMjEzNjY1My4zNjMwMDAyLCJzdWIiOiI2NzNlNGVjZGQ4MDg2ZTY2YjdkYTZiMDQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.dmcVaR_5wZkplTE_-Xgh84GAZUcbxBs6KHLTUbDuBqs"
-const API_KEY = "d98988d21ddcbc6532a484963fdbac7e"
-
-export { screenWidth, screenHeight, token, API_KEY };
